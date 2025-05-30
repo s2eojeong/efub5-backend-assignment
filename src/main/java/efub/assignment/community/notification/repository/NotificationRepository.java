4ec130a226c3b8cc96a657788fbd481f07f53b18
m2nsp <mini071663@gmail.com>
2025-05-30 23:05:55 +0900

    🔧fix: 알림 조회 로직 수정
    - 안 읽은 알림만 조회하도록 수정

--- src/main/java/efub/assignment/community/notification/repository/NotificationRepository.java
+++ src/main/java/efub/assignment/community/notification/repository/NotificationRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findAllByReceiver(Member receiver);
+    List<Notification> findAllByReceiverAndIsReadFalse(Member receiver);
 }
--- src/main/java/efub/assignment/community/notification/service/NotificationService.java
+++ src/main/java/efub/assignment/community/notification/service/NotificationService.java
@@ -3,16 +3,13 @@ package efub.assignment.community.notification.service;
 import efub.assignment.community.member.domain.Member;
 import efub.assignment.community.member.service.MemberService;
 import efub.assignment.community.notification.domain.Notification;
-import efub.assignment.community.notification.domain.NotificationType;
 import efub.assignment.community.notification.dto.response.NotificationListResponseDTO;
 import efub.assignment.community.notification.repository.NotificationRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -25,7 +22,7 @@ public class NotificationService {
         //1. memberId 유효성 검사
         Member receiver = memberService.findMemberByMemberId(memberId);
         //2. 회원에 해당하는 알림 찾기
-        List<Notification> notifications = notificationRepository.findAllByReceiver(receiver);
+        List<Notification> notifications = notificationRepository.findAllByReceiverAndIsReadFalse(receiver);
         //3. 읽음 처리 (isRead = true)
         notifications.forEach(Notification::markAsRead);
         //4. DTO 변환
